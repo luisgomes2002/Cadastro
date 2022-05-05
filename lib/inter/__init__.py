9fd74f13b5a31ebbafb8a6ecb9528ec762b37e7f
luisgomes2002 <luisgmgomes@gmail.com>
2022-05-05 15:53:46 -0300

    save 1

---
+++ lib/inter/__init__.py
@@ -0,0 +1,10 @@
+def lin(tam=42):
+    return '-' * tam
+
+def cabeçalho(txt):
+    print(lin())
+    print(txt.center(42))
+    print(lin())
+
+def menu(lista):
+    print(lista)
---
+++ main.py
@@ -0,0 +1,4 @@
+from lib.inter import * 
+
+cabeçalho('Testando 123')
+menu(['opc01', 'opc02', 'opc03'])
